f8a377a5f70ec897452adc438d5b6f882eda7e9d
Lethe <37437842+wn-na@users.noreply.github.com>
2024-11-16 22:05:30 +0900

    feat: init commit

---
+++ src/index.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { NativeEventEmitter, NativeModules, Platform } from "react-native";
+
+const LINKING_ERROR =
+    `The package 'react-native-is-multi-window' doesn't seem to be linked. Make sure: \n\n` +
+    Platform.select({ ios: "- You have run 'pod install'\n", default: "" }) +
+    "- You rebuilt the app after installing the package\n";
+
+const ReactNativeIsMultiWindowModule = NativeModules.ReactNativeIsMultiWindow
+    ? NativeModules.ReactNativeIsMultiWindow
+    : new Proxy(
+          {},
+          {
+              get() {
+                  throw new Error(LINKING_ERROR);
+              },
+          }
+      );
+
+const ReactNativeIsMultiWindowEmitter = Platform.OS === "ios" || Platform.OS === "android" ? new NativeEventEmitter(ReactNativeIsMultiWindowModule) : undefined;
+
+const addListener = (callback: (isMultiMode: boolean) => void): number => {
+    return 0;
+};
+
+const removeListener = (id: number) => {};
+
+export const isMultiWindowMode = async () => {
+    return true;
+};
+
+export const useMultiWindowMode = () => {
+    const [isMultiMode, setMultiMode] = useState(false);
+
+    useEffect(() => {
+        isMultiWindowMode().then(setMultiMode);
+        const listener = addListener((mode) => {
+            setMultiMode(mode);
+        });
+        return () => {
+            removeListener(listener);
+        };
+    }, []);
+
+    return { isMultiMode, isMultiWindowMode };
+};
